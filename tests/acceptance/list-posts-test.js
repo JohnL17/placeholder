93aef6d7715fe777e7ae59ce5baebb3bc7ee5c48
Jonas Lehmann <jonas.lehmann@adfinis-sygroup.ch>
2018-03-20 14:12:32 +0100

    add routes, templates and acceptance test

---
+++ tests/acceptance/list-posts-test.js
@@ -0,0 +1,22 @@
+import { module, test } from 'qunit'
+import { visit, currentURL, click } from '@ember/test-helpers'
+import { setupApplicationTest } from 'ember-qunit'
+
+module('Acceptance | list posts', function(hooks) {
+  setupApplicationTest(hooks)
+
+  test('should show about as the home page', async function(assert) {
+    await visit('/')
+    assert.equal(currentURL(), '/about', 'should redirect automatically')
+  })
+
+  test('should link to posts page', async function(assert) {
+    await visit('/')
+    await click('.menu-posts')
+    assert.equal(currentURL(), '/posts', 'should navigate to posts')
+  })
+
+  test('should list all posts.', async function(assert) {})
+
+  test('should show details for a selected post.', async function(assert) {})
+})
