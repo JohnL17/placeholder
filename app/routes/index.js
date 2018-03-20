4f0685412d79c17095b2cdd61d362a7e2b05779e
Jonas Lehmann <jonas.lehmann@adfinis-sygroup.ch>
2018-03-20 14:36:05 +0100

    change redirection

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route'
 
 export default Route.extend({
-  beforeModel() {
-    this.replaceWith('about')
+  redirect(model, transition) {
+    this.transitionTo('about')
   }
 })
