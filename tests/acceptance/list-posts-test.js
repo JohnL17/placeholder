09cd910bd88560e611313e4f96af1fc867777138
Jonas Lehmann <jonas.lehmann@adfinis-sygroup.ch>
2018-03-27 10:49:07 +0200

    add new acceptance tests

--- tests/acceptance/list-posts-test.js
+++ tests/acceptance/list-posts-test.js
@@ -25,5 +25,40 @@ module('Acceptance | list posts', function(hooks) {
     )
   })
 
-  test('should show details for a selected post.', async function(assert) {})
+  test('should show details for a selected post.', async function(assert) {
+    await visit('/posts/1')
+    assert.equal(currentURL(), '/posts/1', 'should navigate to show route')
+    assert.ok(
+      this.element
+        .querySelector('.uk-card-title')
+        .textContent.includes(
+          'sunt aut facere repellat provident occaecati excepturi optio reprehenderit'
+        ),
+      'should list post title'
+    )
+    assert.equal(
+      this.element.querySelector('p').innerHTML,
+      'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+      'should list post body'
+    )
+  })
+
+  test('should list all albums.', async function(assert) {
+    await visit('/gallery')
+    assert.equal(
+      this.element.querySelectorAll('.uk-card').length,
+      100,
+      'should display 100 listings'
+    )
+  })
+
+  test('should list all photos for selected album.', async function(assert) {
+    await visit('/gallery/1')
+    assert.equal(currentURL(), '/gallery/1', 'should navigate to show route')
+    assert.equal(
+      this.element.querySelectorAll('img').length,
+      50,
+      'should display 50 photos'
+    )
+  })
 })
