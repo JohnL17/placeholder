65f46dc17185107d4c8ee7331e724b9ad817a65a
Jonas Lehmann <jonas.lehmann@adfinis-sygroup.ch>
2018-03-27 12:51:16 +0200

    rewrite acceptance tests with qunit-dom

--- tests/acceptance/list-posts-test.js
+++ tests/acceptance/list-posts-test.js
@@ -6,59 +6,66 @@ module('Acceptance | list posts', function(hooks) {
   setupApplicationTest(hooks)
 
   test('should show about as the home page', async function(assert) {
+    assert.expect(1)
+
     await visit('/')
+
     assert.equal(currentURL(), '/about', 'should redirect automatically')
   })
 
   test('should link to posts page', async function(assert) {
+    assert.expect(1)
+
     await visit('/')
+
     await click('.menu-posts')
+
     assert.equal(currentURL(), '/posts', 'should navigate to posts')
   })
 
   test('should list all posts.', async function(assert) {
+    assert.expect(1)
+
     await visit('/posts')
-    assert.equal(
-      this.element.querySelectorAll('.uk-card').length,
-      100,
-      'should display 100 listings'
-    )
+
+    assert.dom('.uk-card').exists({ count: 100 })
   })
 
   test('should show details for a selected post.', async function(assert) {
+    assert.expect(3)
+
     await visit('/posts/1')
+
     assert.equal(currentURL(), '/posts/1', 'should navigate to show route')
-    assert.ok(
-      this.element
-        .querySelector('.uk-card-title')
-        .textContent.includes(
-          'sunt aut facere repellat provident occaecati excepturi optio reprehenderit'
-        ),
-      'should list post title'
-    )
-    assert.equal(
-      this.element.querySelector('p').innerHTML,
-      'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-      'should list post body'
-    )
+
+    assert
+      .dom('.uk-card-title')
+      .hasText(
+        'sunt aut facere repellat provident occaecati excepturi optio reprehenderit'
+      )
+
+    assert
+      .dom('p')
+      .hasText(
+        'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+      )
   })
 
   test('should list all albums.', async function(assert) {
+    assert.expect(1)
+
     await visit('/gallery')
-    assert.equal(
-      this.element.querySelectorAll('.uk-card').length,
-      100,
-      'should display 100 listings'
-    )
+
+    assert.dom('.uk-card').exists({ count: 100 })
   })
 
   test('should list all photos for selected album.', async function(assert) {
+    assert.expect(2)
+
     await visit('/gallery/1')
+
     assert.equal(currentURL(), '/gallery/1', 'should navigate to show route')
-    assert.equal(
-      this.element.querySelectorAll('img').length,
-      50,
-      'should display 50 photos'
-    )
+
+    assert.dom('img').exists({ count: 50 })
   })
 })
