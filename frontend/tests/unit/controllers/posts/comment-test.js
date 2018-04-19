9b145fffe640c9dcfb010053b09b7972f0fefadb
Jonas Lehmann <jonas.lehmann@adfinis-sygroup.ch>
2018-04-19 17:16:17 +0200

    fixed tests format

--- frontend/tests/unit/controllers/posts/comment-test.js
+++ frontend/tests/unit/controllers/posts/comment-test.js
@@ -1,12 +1,12 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { module, test } from "qunit";
+import { setupTest } from "ember-qunit";
 
-module('Unit | Controller | posts/comment', function(hooks) {
+module("Unit | Controller | posts/comment", function(hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('it exists', function(assert) {
-    let controller = this.owner.lookup('controller:posts/comment');
+  test("it exists", function(assert) {
+    let controller = this.owner.lookup("controller:posts/comment");
     assert.ok(controller);
   });
 });
