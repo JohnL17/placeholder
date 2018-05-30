10feed135682ee999fa0271bc5ad601ec24f6cb1
Jonas Lehmann <jonas.lehmann@adfinis-sygroup.ch>
2018-05-30 14:03:04 +0200

    simplify code blocks

--- frontend/app/controllers/posts/edit.js
+++ frontend/app/controllers/posts/edit.js
@@ -1,19 +1,13 @@
 import Controller from "@ember/controller";
 
 export default Controller.extend({
-  init() {
-    this._super(...arguments);
-  },
   actions: {
     editPost() {
-      let post = this.get("model");
-      post.setProperties({
-        title: this.get("model.title"),
-        body: this.get("model.body")
-      });
-      post.save().then(() => {
-        this.transitionToRoute("posts.index");
-      });
+      this.get("model")
+        .save()
+        .then(() => {
+          this.transitionToRoute("posts.index");
+        });
     }
   }
 });
